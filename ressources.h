a503620cc8e1326b1e4290deb3668d6cfa382c94
Lucien <Saperli000@gmail.com>
2020-04-16 21:57:34 +0200

    Fonctions commentées

--- ressources.h
+++ ressources.h
@@ -16,18 +16,22 @@ void stop() {
   delay(1000);
 }
 
+// Tire sur la ficelle avec le moteur Cata
 void chargerCatapulte() {
 
 }
 
+// Relache la ficelle avec le moteur Cata
 void envoyerCatapulte() {
 
 }
 
+// Lis valeurs des photorésistances et en déduis la direction à prendre
 int getDirection() {
   return 0;
 }
 
+// Rêgle la puissance des moteurs Roues pour avance selon la direction transmise
 void avancer(int direction) {
-  
+
 }
